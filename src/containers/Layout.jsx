cda4f5e03493ebbefb73aa7c961e5d5d35b48b5b
Tien <tien.huynh@gemi.io>
2022-09-12 13:43:24 +0700

    rename cache file

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../components/header";
+import Header from "../components/AppHeader";
 import Container from 'react-bootstrap/Container';
 
 function Layout() {
