a915d00d3b41f5bd31918cc699ff46bf9a31e8be
Tien <tien.huynh@gemi.io>
2022-09-12 13:49:06 +0700

    remove unused variable

--- src/containers/MoviePage.jsx
+++ src/containers/MoviePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import Movies from "../components/MovieList";
 import { getMovies } from "../actions/movieActions";
